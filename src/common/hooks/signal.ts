01fa2c440edd7afb32352b2ad7f7706f300085fb
Susan Cheng <susan.doggie@gmail.com>
2025-03-01 23:59:46 +0800

    Update signal.ts

--- src/common/hooks/signal.ts
+++ src/common/hooks/signal.ts
@@ -27,16 +27,13 @@ import _ from 'lodash';
 import { reconciler } from '../../reconciler/reconciler';
 import { Signal } from '../types/signal';
 
-export const useSignal = <T, R = T>(
-  signal: Signal<T>,
-  selector: (state: T) => R = v => v as any
-) => {
+export const useSignal = <T>(signal: Signal<T>) => {
   if (reconciler.registry.get(signal) !== 'SIGNAL') throw Error(`Invalid type of ${signal}`);
   const state = reconciler.currentHookState;
   if (!state) throw Error('useContext must be used within a render function.');
   const { onStateChange, dispose } = state;
   dispose.push(signal.subscribe((oldVal, newVal) => {
-    if (_.isEqual(selector(oldVal), selector(newVal))) onStateChange();
+    if (_.isEqual(oldVal, newVal)) onStateChange();
   }));
   return [signal.value, signal.setValue] as const;
 }
