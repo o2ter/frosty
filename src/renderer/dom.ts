227b1ace69a95d70e865e442949dfd95e57f3131
Susan Cheng <susan.doggie@gmail.com>
2025-03-10 23:36:50 +0800

    Update dom.ts

--- src/renderer/dom.ts
+++ src/renderer/dom.ts
@@ -31,7 +31,7 @@ export class DOMRenderer {
 
   createRoot(root: HTMLElement) {
     let state: ReturnType<typeof reconciler.buildVNodes> | undefined;
-    let currentNodes: VNode[];
+    let currentNodes: VNode[] | undefined;
     return {
       mount: (component: ComponentNode) => {
         state = reconciler.buildVNodes(component);
