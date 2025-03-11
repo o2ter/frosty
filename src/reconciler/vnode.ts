c221a1e955d457e9fba0ee1218d3b8ba40b7d43d
Susan Cheng <susan.doggie@gmail.com>
2025-03-11 23:40:49 +0800

    Update vnode.ts

--- src/reconciler/vnode.ts
+++ src/reconciler/vnode.ts
@@ -71,7 +71,7 @@ export class VNode {
   updateIfNeed(options: {
     contextValue: Map<Context<any>, any>;
   }) {
-    if (!this._dirty) return;
+    if (_.every([...this._listens], ([k, v]) => _.isEqual(options.contextValue.get(k), v)) && !this._dirty) return;
     try {
       const { type, props } = this._component;
       let children: (VNode | string)[];
