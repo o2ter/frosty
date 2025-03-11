70c9595c7d867d34415858ed973a3226b260eb13
Susan Cheng <susan.doggie@gmail.com>
2025-03-11 23:49:52 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -38,3 +38,4 @@ export { Context, ContextType, createContext } from './common/types/context';
 export { Fragment } from './common/types/fragment';
 export { _ElementType as ElementType } from './common/types/jsx';
 export { mergeRefs } from './common/utils';
+export { DOMRenderer } from './renderer/dom';
