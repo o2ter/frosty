61251753b9469e72407cf821bf58c82d73a93ee1
Susan Cheng <susan.doggie@gmail.com>
2025-03-12 23:05:31 +0800

    test

---
+++ tests/server/main/index.tsx
@@ -0,0 +1,14 @@
+import { DOMRenderer } from '~/renderer/dom';
+
+const root = document.getElementById('root');
+
+const App = () => {
+
+  return (
+    <div>
+      <span>test</span>
+    </div>
+  );
+};
+
+DOMRenderer.createRoot(root!).mount(<App />);
