dde968ca5f1eaaab927e5eecf17d54894a62041e
Susan Cheng <susan.doggie@gmail.com>
2025-03-13 22:38:17 +0800

    Update index.tsx

--- tests/server/main/index.tsx
+++ tests/server/main/index.tsx
@@ -1,11 +1,17 @@
+import { useEffect, useState } from '~/index';
 import { DOMRenderer } from '~/renderer/dom';
 
 const root = document.getElementById('root');
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    const handle = setInterval(() => { setCounter(v => v + 1) }, 1000);
+    return () => clearTimeout(handle);
+  }, []);
   return (
     <div>
-      <span>test</span>
+      <span>counter: {counter}</span>
     </div>
   );
 };
