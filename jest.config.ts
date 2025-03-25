1f7bc3c0172f413de8fa16f8aa64de316dfe354b
Susan Cheng <susan.doggie@gmail.com>
2025-03-25 21:59:41 +0800

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,11 @@
 export default {
+  setupFiles: [
+    "<rootDir>/node_modules/core-js",
+  ],
   moduleNameMapper: {
     "~/(.*)": "<rootDir>/src/$1",
   },
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   }, 
-  setupFiles: [
-    "<rootDir>/node_modules/core-js"
-  ],
 };
