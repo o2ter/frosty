f778f13a9b09b7f52020361a1f2b89ae8093a29a
Susan Cheng <susan.doggie@gmail.com>
2025-04-02 21:43:27 +0800

    Update App.tsx

--- tests/server/main/App.tsx
+++ tests/server/main/App.tsx
@@ -9,7 +9,17 @@ export const App = () => {
     return () => clearTimeout(handle);
   }, []);
   return (
-    <div style={{ padding: 64, margin: 16 }} inlineStyle={{ padding: 32, margin: 16 }}>
+    <div
+      style={{
+        padding: 64,
+        margin: 16,
+        backgroundColor: `rgb(${counter % 256}, 0, 0)`,
+      }}
+      inlineStyle={{
+        padding: 32,
+        margin: 16,
+      }}
+    >
       <table bgColor='aliceblue'>
         {_.map(_.range(2), i => <tr>
           {_.map(_.range(2), j => <td>{i * counter + j}</td>)}
