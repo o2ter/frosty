85be813e6c9b3300e2737ccec294e9826f2adb5c
Susan Cheng <susan.doggie@gmail.com>
2026-02-08 01:19:18 +0800

    Update renderer.ts

--- src/core/renderer.ts
+++ src/core/renderer.ts
@@ -90,8 +90,10 @@ export abstract class _Renderer<T> {
             if (element) {
               if (filter && !filter(child)) continue;
               yield element;
-            } else {
+            } else if (filter && !filter(child)) {
               yield* children(child, filter);
+            } else {
+              yield* children(child);
             }
           }
         }
