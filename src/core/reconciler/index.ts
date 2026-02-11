216047076136131db1b234942582501ba8af43a5
Susan Cheng <susan.doggie@gmail.com>
2026-02-11 23:39:52 +0800

    Update index.ts

--- src/core/reconciler/index.ts
+++ src/core/reconciler/index.ts
@@ -185,7 +185,6 @@ export class VNode {
       if (_.isString(type) || type?.prototype instanceof NativeElementType) {
         children = _resolve_children(props.children);
         native = this;
-        yield { mount: this };
       } else if (isContext(type)) {
         const { value } = props;
         if (!equalDeps(this.#content_value, value)) {
@@ -214,7 +213,6 @@ export class VNode {
           }
           await Promise.all(state.tasks);
         }
-        yield { mount: this };
       } else {
         throw Error(`Invalid node type ${type}`);
       }
@@ -260,12 +258,12 @@ export class VNode {
           console.error(e);
         }
       })();
-      yield { mount: this };
       if (this.#nativeParent && this.#nativeParent !== this) {
         yield { mount: this.#nativeParent };
       }
     } finally {
       reconciler._currentHookState = undefined;
+      yield { mount: this };
     }
   }
 
