30066d66dfc497d2143c8591458b3eec7d621a98
Susan Cheng <susan.doggie@gmail.com>
2026-02-13 12:45:36 +0800

    Update gen-data.mjs

--- scripts/gen-data.mjs
+++ scripts/gen-data.mjs
@@ -197,7 +197,7 @@ while (true) {
     if (common_attrs.length === 0) continue;
     item.attributes = _.uniq([...item.attributes || [], ...common_attrs]);
     for (const type of ['HTML', 'SVG']) {
-      if (_.startsWith(name, type) || _.startsWith(name, 'Element')) {
+      if (_.startsWith(name, type) || _.every(['HTML', 'SVG'], k => !_.startsWith(name, k))) {
         for (const attrs of common_attrs) {
           for (const found of _.filter(_.values(mapped), x => _.startsWith(x.name, type))) {
             const picked = _.pickBy(found.properties, (v, k) => _.camelCase(k).toLowerCase() === _.camelCase(attrs).toLowerCase());
